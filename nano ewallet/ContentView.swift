e423236e5be443ad53677b3e99cb4ef3638ecc02
khieudangbk <khieudangngoc@gmail.com>
2026-07-30 21:53:49 +0700

    Initial Commit

---
+++ nano ewallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nano ewallet
+//
+//  Created by Le Tran Nhu Y on 30/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nano ewallet/nano_ewalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  nano_ewalletApp.swift
+//  nano ewallet
+//
+//  Created by Le Tran Nhu Y on 30/7/26.
+//
+
+import SwiftUI
+
+@main
+struct nano_ewalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
